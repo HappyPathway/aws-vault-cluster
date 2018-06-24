7f6ec57a7cb04f43e291f969a992fc1c619febe2
Dave Arnold <darnold@hashicorp.com>
2018-06-23 22:57:23 -0700

    bumping version

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ provider "aws" {
 // Modules
 module "vault_cluster" {
   source              = "app.terraform.io/Darnold-Hashicorp/vault-cluster/aws"
-  version             = "1.2.0"
+  version             = "1.2.1"
   key_name            = "${var.key_name}"
   servers             = "${var.servers}"
   subnet              = "${data.terraform_remote_state.network.public_subnet}"
