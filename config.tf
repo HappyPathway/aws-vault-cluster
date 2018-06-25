f4f9d2a135f41f3070c9b0f875dcf3bded1ccefd
Dave Arnold <darnold@hashicorp.com>
2018-06-25 11:37:32 -0700

    removing template file from config.tf

--- config.tf
+++ config.tf
@@ -28,11 +28,3 @@ data "terraform_remote_state" "consul_cluster" {
 data "aws_instance" "consul_instance" {
   instance_id = "${data.terraform_remote_state.consul_cluster.instances[0]}"
 }
-
-data "template_file" "vault_config" {
-  template = "${file("vault_config.hcl.tpl")}"
-
-  vars {
-    consul_address = "${data.aws_instance.consul_instance.private_ip}"
-  }
-}
